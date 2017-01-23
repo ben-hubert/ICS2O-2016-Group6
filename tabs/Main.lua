0e9f0bf8c568dddcfe4d83e005f1f9332af58d0a
Chibalion at codea-scm <Chibalion@codea-scm.aws.mapote.com>
2017-01-23 16:17:32 +0000

    settings scene

--- tabs/Main.lua
+++ tabs/Main.lua
@@ -19,6 +19,7 @@ worldSelected = nil
 levelSelected = nil
 backgroundColor = color(178, 178, 178, 255)
 local legendbackground
+backMusic = nil
 
 -- local variables to this scene
 
@@ -46,9 +47,10 @@ function setup() -- set up display options
     Scene("mainWorldScene", MainWorldScene)
     Scene("afterLevelWinScene", AfterLevelWinScene)
     Scene("afterLevelFailScene",AfterLevelFailScene)
+    Scene("settingsScene", SettingsScene)
 
     Scene.Change("companyLogoScene")
-    
+    backMusic = music("Game Music One 2:Venus", 10000000)
 end
 
 -- This function gets called once every frame
